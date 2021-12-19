351eafb43036b7479fae7c1d6c936c7b0c6d873a
Denis Chernykh <denrus38@hotmail.com>
2021-12-19 14:46:40 +0700

    Removed extra code in App.js

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
-import {Alert, StyleSheet} from "react-native";
+import {StyleSheet} from "react-native";
 import {Provider} from "react-redux";
-import {store} from "./store/store";
 import {NavigationContainer} from "@react-navigation/native";
+
 import {MyStack} from "./router/MyStack";
 
+import {store} from "./store/store";
+
 export default function App() {
-
-  const handleButtonPress = () => {
-    Alert.alert("Создание нового поста", "Какое то сообщение", [
-      {text: "Отменить"},
-      {text: "Создать"}
-    ])
-  }
-
   return (
     <Provider store={store}>
       <NavigationContainer>
-        {/*<SafeAreaView style={styles.container}>*/}
           <MyStack />
-
-          {/*<MainScreen />*/}
-
-          {/*<Text>Список постов:</Text>*/}
-          {/*<Button title={'Создать новый пост'} onPress={handleCreateNewPost} />*/}
-          {/*<StatusBar style="auto"/>*/}
-        {/*</SafeAreaView>*/}
       </NavigationContainer>
     </Provider>
   );
@@ -34,8 +20,5 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'column',
-    // alignItems: 'center',
-    // paddingTop: 50,
-    // backgroundColor: "#fff",
   },
 });
